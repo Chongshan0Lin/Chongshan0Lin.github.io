88ac7c5d983fb70c37b6efe6087b2d36c003cdfa
Chongshan0Lin <chongshan0lin@gmail.com>
2024-07-01 17:32:21 +0800

    abaaba

--- src/main.js
+++ src/main.js
@@ -1,4 +1,14 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false
+
+Vue.use(Vuesax, {})
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
